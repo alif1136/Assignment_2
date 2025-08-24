ae932fe22385996ccba04db08dfc43a7627f2a4e
Sweet-Potlet <alifhamim50@gmail.com>
2025-08-25 01:30:03 +0600

    init commit

--- Assignment_2.dart
+++ Assignment_2.dart
@@ -1,45 +1,19 @@
-// import 'dart:io';
-//
-// void main()
-// {
-//   var array = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
-//  for (int i = 0; i < array.length; i++)
-//  {
-//    print('month = ${array[i]}');
-//  }
-// print('Enter a number from 0 to 11:');
-// int ? index = int.tryParse(stdin.readLineSync()!)!;
-//
-// print('Month = ${array[index]}');
-// }
-
-// Abstract class Animal
 abstract class Animal {
-  // Private field
+
   String _name;
-
-  // Constructor
   Animal(this._name);
-
-  // Abstract method
   void makeSound();
-
-  // Concrete method
   String getName() {
     return _name;
   }
 }
-
-// Lion class
 class Lion extends Animal {
-  double _maneSize; // private property
+  double _maneSize;
 
   Lion(String name, this._maneSize) : super(name);
 
-  // Getter
   double get maneSize => _maneSize;
 
-  // Setter with validation
   set maneSize(double size) {
     if (size >= 0) {
       _maneSize = size;
@@ -54,16 +28,14 @@ class Lion extends Animal {
   }
 }
 
-// Elephant class
+
 class Elephant extends Animal {
-  double _trunkLength; // private property
+  double _trunkLength;
 
   Elephant(String name, this._trunkLength) : super(name);
 
-  // Getter
   double get trunkLength => _trunkLength;
 
-  // Setter with validation
   set trunkLength(double length) {
     if (length >= 0) {
       _trunkLength = length;
@@ -78,16 +50,13 @@ class Elephant extends Animal {
   }
 }
 
-// Parrot class
 class Parrot extends Animal {
-  int _vocabularySize; // private property
+  int _vocabularySize;
 
   Parrot(String name, this._vocabularySize) : super(name);
 
-  // Getter
   int get vocabularySize => _vocabularySize;
 
-  // Setter with validation
   set vocabularySize(int size) {
     if (size >= 0) {
       _vocabularySize = size;
@@ -103,15 +72,13 @@ class Parrot extends Animal {
 }
 
 void main() {
-  // Create a list of animals
+
   List<Animal> zoo = [];
 
-  // Add animals
   zoo.add(Lion("Leo", 25.0));
   zoo.add(Elephant("Ella", 150.5));
   zoo.add(Parrot("Polly", 100));
 
-  // Loop through animals
   for (var animal in zoo) {
     print("Animal: ${animal.getName()}");
 
@@ -122,16 +89,13 @@ void main() {
     } else if (animal is Parrot) {
       print("Vocabulary Size: ${animal.vocabularySize} words");
     }
-
     print("Sound: ");
     animal.makeSound();
     print("  ");
   }
-
-  // Example validation check
   Parrot badParrot = Parrot("Jack", -5);
   print("Parrot Vocabulary (before correction): ${badParrot.vocabularySize}");
-  badParrot.vocabularySize = -10; // should trigger validation
-  badParrot.vocabularySize = 50; // valid update
+  badParrot.vocabularySize = -10;
+  badParrot.vocabularySize = 50;
   print("Parrot Vocabulary (after correction): ${badParrot.vocabularySize}");
 }
